32bcfe176d166ffa6b7bc87bf7932e00c21f9b9e
AndyLocks <jandylokc@gmail.com>
2023-07-10 22:20:40 +0200

    удаление не нужной функции

--- pars.py
+++ pars.py
@@ -1,27 +1,12 @@
-import config
+from config import url, headers
 import requests
 from bs4 import BeautifulSoup
 
 
-def parser(index, url = config.url, headers = config.headers):
-    req = requests.get(url, headers=headers)
-    soup = BeautifulSoup(req.content, "html.parser")
-    crypto_currencies = soup.find_all("tr")
-    info = crypto_currencies[index].find_all("td")
-    price = info[3].find("span")
-    market_cup = info[7].find_all("span")
-    value = info[8].find("p")
-    return price, market_cup[1], value
-
-
-
-
-
-
 class Parser:
         
-    __url = config.url
-    __headers = config.headers
+    __url = url
+    __headers = headers
     
     def getAllInfo(name: str) -> dict[str, str]:
 
